f290cb4b3406e9ad00b71ce871a0ae6b402c5f2c
marouane youssef <marouane_youssef@gcarrefour.com>
2017-10-23 01:40:06 +0200

    generique URL

--- SLProject/Manager/API.swift
+++ SLProject/Manager/API.swift
@@ -7,14 +7,18 @@
 //
 
 import Foundation
+let API_BASE_URL = ""
+let API_KEY = ""
+
 typealias APIRequestResponse = (Data?, URLResponse?, Error?) -> ()
 struct API {
     
     static func loadData(fromUrl url:String? , response : @escaping APIRequestResponse) {
         
-        guard let url  = url ,  let requestURL = URL(string: url) else {
+        guard let requestURL = API.generateCurrentRequestURL(param: "user") else {
             return
         }
+        
         let urlRequest = URLRequest(url: requestURL)
         
         let task = URLSession.shared.dataTask(with: urlRequest, completionHandler: response)
@@ -22,55 +26,39 @@ struct API {
     }
     
     static func login_now(username:String, password:String , url : String? , response: @escaping APIRequestResponse) {
-    
+        
         guard let url  = url ,  let requestURL = URL(string: url) else {
             return
         }
         let urlRequest = NSMutableURLRequest(url: requestURL)
         urlRequest.httpMethod = "POST"
         urlRequest.cachePolicy = NSURLRequest.CachePolicy.reloadIgnoringCacheData
-
+        
         let dict = ["username" : username,
                     "password" : password]
         
         var paramString = ""
         for (key,value) in dict {
-          paramString += key + "=" + value + "&"
+            paramString += key + "=" + value + "&"
         }
         urlRequest.httpBody = paramString.data(using: String.Encoding.utf8)
-       
+        
         let task = URLSession.shared.dataTask(with: urlRequest as URLRequest, completionHandler: response)
         task.resume()
-       
-    
+        
+        
     }
 }
 
 extension API {
     
-    private struct Constants {
-        static let APPID = ""
-        static let baseURL = ""
-    }
-    
-    enum ResourcePath: String {
-        case service = ""
-        
-        var path: String {
-            return Constants.baseURL + rawValue
-        }
-        
-        var forecastpath: String {
-            return Constants.baseURL + rawValue + Constants.APPID
-        }
-    }
-    
-    static func generateCurrentRequestURL() -> URL? {
-        guard var components = URLComponents(string:"") else {
+    static func generateCurrentRequestURL(param : String) -> URL? {
+        guard var components = URLComponents(string:API_BASE_URL) else {
             return nil
         }
         
-        components.queryItems = [URLQueryItem(name:"lat", value:"")]
+        components.queryItems = [URLQueryItem(name:"Key", value:param),
+                                 URLQueryItem(name:"key", value:API_KEY)]
         return components.url
     }
 }
